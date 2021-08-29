647f8fdf1239432360e432703506496bfa89fac6
jclarson <jclarson@hotmail.com>
2021-08-29 15:54:46 -0500

    updates blackjack

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,4 +1,5 @@
 /* #region variables*/
+const LOGGING = false;
 const SUITS = ["♠", "♣", "♥", "♦"];
 const CARDWIDTH = 85;
 const CARDSLOT = CARDWIDTH + 10;
@@ -21,69 +22,84 @@ let game = {};
 let dealer = {}
 
 let player = {
-  name: '',
-  chips: 0,
+  name: 'Player1',
+  chips: 0
 }
 /* #endregion */
 
 initializeGame();
 
-if (player.name == '') { playerEl.style.display = "none" }
+if (player.name == 'Player1') playerEl.style.display = "none";
 
-function hideElement(element) {
-  element.style.visibility = 'hidden';
-}
-
-function showElement(element) {
-  element.style.visibility = 'visible';
-}
-
-function hideCardButtons() {
-  showElement(startButton);
-  hideElement(hitButton);
-  hideElement(standButton);
-}
-
-function shuffleDeck() {
-  let newDeck = [];
-  let cardCount = deck.length;
-  for (let i = 0; i < cardCount; i++) {
-    let cardNumber = Math.floor(Math.random() * deck.length);
-    newDeck.push(deck.splice(cardNumber, 1)[0]);
+function checkDealerHand() {
+  log('inside checkDealerHand');
+  for (let card of dealer.hand) {
+    card.isFaceDown = false;
   }
-  return newDeck;
+  displayCards(dealer, dealerEl);
+  if (!player.isAlive) return;
+  if (player.hasBlackjack) return;
+  log('player is still alive. Continuing in checkDealerHand');
+  while (dealer.total < 17) {
+    if (dealer.totalWithAce <= 17 || dealer.totalWithAce > 21) {
+      log(`Dealer hits with ${dealer.total}/${dealer.totalWithAce}`);
+      dealerHit();
+    } else dealer.total = dealer.totalWithAce;
+    displayCards(dealer, dealerEl);
+  }
+
+  if (dealer.total > 21) {
+    result('win', `Win! (D:Busted)`);
+    return;
+  }
+  if (dealer.total === player.total) {
+    result('push', `Push: (${dealer.total})`);
+    return;
+  }
+
+  if (dealer.total < player.total) result('win', `Win! (D:${dealer.total})`);
+  else result('lose', `Lose: (D:${dealer.total})`);
 }
 
-function initializePerson(person) {
-  person.hand = [];
-  person.has21 = false;
-  person.hasAce = false;
-  person.hasBlackjack = false;
-  person.isBusted = false;
-  person.total = 0;
-  person.totalWithAce = 0;
+function checkForBlackJack() {
+  log('inside checkForBlackJack');
+  if (!dealer.hasBlackjack && !player.hasBlackjack) return false;
+  if (dealer.hasBlackjack) {
+    if (!player.hasBlackjack) result('lose', "Lose: (D:BJ)");
+    else result('push', "Push: (BJ)");
+  } else if (player.hasBlackjack) {
+      result('win', "Win! Blackjack!")
+      sumEl.textContent = `Sum: ${player.totalWithAce}`
+  }
+
+  displayCards(player, cardsEl);
+  checkDealerHand();
+  return true;
 }
 
-function initializeGame() {
-  initializePerson(dealer);
-  initializePerson(player);
-  dealer.name = 'Dealer';
-  dealer.isDealer = true;
-  player.isDealer = false;
-  player.isStanding = false;
-  player.isAlive = false;
-  cardsEl.style.width = dealerEl.style.width = (CARDSLOT * 2) + 'px';
-  deck = initializeDeck();
-  clearCardDisplay(cardsEl);
-  clearCardDisplay(dealerEl);
-  hideCardButtons();
-  hideElement(cardsEl);
-  hideElement(dealerEl);
-  hideElement(dealerText);
-  hideElement(playerText);
-  hideElement(sumEl);
-  sumEl.textContent = `Sum: ${player.total}`;
-  game.isInitialized = true;
+function checkPlayerHand() {
+  if (player.has21) {
+    message = "You've got 21!"
+    checkDealerHand();
+    hideCardButtons();
+  } else if (player.total < 21 || (player.hasAce && player.totalWithAce < 21)) {
+    messageEl.textContent = "Hit or Stand?";
+    showElement(hitButton);
+    showElement(standButton);
+    log('continuing in checkPlayerHand');
+  } else {
+    result('lose', "Lose: Busted");
+    checkDealerHand();
+  }
+
+  if (player.hasAce) {
+    if (player.isStanding || player.has21) sumEl.textContent = `Sum: ${player.totalWithAce}`;
+    else if (player.totalWithAce < 21 && !player.isStanding) sumEl.textContent = `Sum: ${player.total}/${player.totalWithAce}`;
+    else sumEl.textContent = `Sum: ${player.total}`; // (player.totalWithAce > 21) 
+  }
+  else sumEl.textContent = `Sum: ${player.total}`;
+  
+  displayCards(player, cardsEl);
 }
 
 function clearCardDisplay(element) {
@@ -93,27 +109,13 @@ function clearCardDisplay(element) {
   }
 }
 
-function startGame() {
-  //if (!player.hasBlackjack && !player.has21 && player.isAlive) return;
-  if (!game.isInitialized) {
-    initializeGame();
-  }
-  game.isInitialized = false;
-  player.isAlive = true;
-  hideElement(startButton);
-  shuffledDeck = shuffleDeck();
-  dealCard(player);
-  dealCard(dealer); //'rigged', 0 is AofS -- 12 is KofS
-  dealCard(player);
-  dealCard(dealer);
-  renderGame();
-}
-
 function dealCard(person, dealType='normal', i) {
   let card;
   if (dealType == 'normal') card = shuffledDeck.pop();
-  else card = initializeDeck()[i];
-    //console.log(`dealtype is ${dealType} and card is ${card.text}`);
+  else {
+    card = initializeDeck()[i];
+    log(`RIGGED! Dealing ${card.text} to ${person.name}`);
+  }
   if (card.isFaceCard) card.value = 10;
   if (card.isAce) {
     card.value = 1;
@@ -128,69 +130,14 @@ function dealCard(person, dealType='normal', i) {
   }
   if (person.has21 && person.hand.length == 1) person.hasBlackjack = true;
   if (person.total > 21) person.isBusted = true;
-  //console.log(`${person.name} has21: ${person.has21}`);
-  //console.log(`${person.name} hasBlackjack: ${person.hasBlackjack}`)
   if (person.isDealer && person.hand.length === 0) {
     card.isFaceDown = true;
   }
   person.hand.push(card);
 }
 
-function checkForBlackJack() {
-  //console.log('inside checkForBlackJack');
-  if (!dealer.hasBlackjack && !player.hasBlackjack) return false;
-  if (dealer.hasBlackjack) {
-    //console.log('dealer has blackjack');
-    if (!player.hasBlackjack) message = "Dealer has Blackjack.  You lose.";
-    else message = "You and Dealer have Blackjack.  You Push.";
-  } else if (player.hasBlackjack) {
-      message = "You've got Blackjack!";
-      sumEl.textContent = `Sum: ${player.totalWithAce}`
-  }
-
-  player.isAlive = false;
-  displayCards(player, cardsEl);
-  checkDealerHand();
-  messageEl.textContent = message;
-  hideCardButtons();
-  return true;
-}
-
-function renderGame() {
-  //console.log('inside renderGame');
-  displayCards(dealer, dealerEl);
-  let gameOver = checkForBlackJack();
-  if (gameOver) return;
-
-  checkPlayerHand();
-}
-
-function checkPlayerHand() {
-  if (player.has21) {
-    message = "You've got 21!"
-    checkDealerHand();
-    hideCardButtons();
-  } else if (player.total < 21 || (player.hasAce && player.totalWithAce < 21)) {
-    message = "Do you want another card?";
-    showElement(hitButton);
-    showElement(standButton);
-  } else {
-    message = "Sorry, you busted.";
-    player.isAlive = false;
-    checkDealerHand();
-    hideCardButtons();
-  }
-
-  if (player.hasAce) {
-    if (player.isStanding || player.has21) sumEl.textContent = `Sum: ${player.totalWithAce}`;
-    else if (player.totalWithAce < 21 && !player.isStanding) sumEl.textContent = `Sum: ${player.total}/${player.totalWithAce}`;
-    else sumEl.textContent = `Sum: ${player.total}`; // (player.totalWithAce > 21) 
-  }
-  else sumEl.textContent = `Sum: ${player.total}`;
-  
-  messageEl.textContent = message;
-
-  displayCards(player, cardsEl);
+function dealerHit() {
+  dealCard(dealer);
 }
 
 function displayCards(person, cardHolder) {
@@ -200,7 +147,6 @@ function displayCards(person, cardHolder) {
   showElement(playerEl);
   showElement(cardHolder);
   if (!dealer.hasBlackjack) showElement(sumEl);
-  //console.log('calling ccd 1 time from displayCards');
   clearCardDisplay(cardHolder);
   cardHolder.style.width = (person.hand.length * CARDSLOT) + 'px';
   for (let card of person.hand) {
@@ -222,70 +168,23 @@ function displayCards(person, cardHolder) {
   }
 }
 
+function hideCardButtons() {
+  showElement(startButton);
+  hideElement(hitButton);
+  hideElement(standButton);
+}
+
+function hideElement(element) {
+  element.style.visibility = 'hidden';
+}
+
 function hit() {
   if (player.hasBlackjack || player.has21 || player.isStanding || !player.isAlive) return;
+  log(`player hits with ${player.total}/${player.totalWithAce}`);
   dealCard(player);
+  log(`player now has ${player.total}/${player.totalWithAce}`);
+  
   checkPlayerHand();
-  message = "Drawing a new card from the deck";
-}
-
-function dealerHit() {
-  dealCard(dealer);
-}
-
-function stand() {
-  if (player.hasBlackjack || player.has21 || player.isStanding || !player.isAlive) return;
-  player.isStanding = true;
-  if (player.hasAce) {
-    if (player.totalWithAce > 21) player.totalWithAce = player.total;
-    else player.total = player.totalWithAce;
-  }
-  //message = `Player stands on ${player.total}`;
-  //messageEl.textContent = message;
-  checkPlayerHand();
-  checkDealerHand();
-}
-
-function checkDealerHand() {
-  //console.log('inside checkDealerHand');
-  for (let card of dealer.hand) {
-    card.isFaceDown = false;
-  }
-  displayCards(dealer, dealerEl);
-  if (!player.isAlive) return;
-  if (player.hasBlackjack) return;
-
-  while (dealer.total < 17) {
-    if (dealer.totalWithAce <= 17 || dealer.totalWithAce > 21) {
-      console.log(`Dealer has ${dealer.total}.  Dealer hits.`);
-      dealerHit();
-    } else dealer.total = dealer.totalWithAce;
-    displayCards(dealer, dealerEl);
-  }
-  if (dealer.total > 21) {
-    message = `The Dealer busts with ${dealer.total}.  You Win!`;
-    player.isAlive = false;
-    messageEl.textContent = message;
-    hideCardButtons();
-    return;
-  }
-  if (dealer.total === player.total) {
-    message = `You and Dealer both have ${dealer.total}.  You Push.`;
-    player.isAlive = false;
-    messageEl.textContent = message;
-    hideCardButtons();
-    return;
-  }
-  if (dealer.total < player.total) {
-    message = `Dealer has ${dealer.total}.  You Win!`;
-    player.isAlive = false;
-    messageEl.textContent = message;
-  } else {
-    message = `Dealer has ${dealer.total}.  You Lose.`;
-    player.isAlive = false;
-    messageEl.textContent = message;
-  }
-  hideCardButtons();
 }
 
 function initializeDeck() {
@@ -331,3 +230,100 @@ function initializeDeck() {
   return deck;
 }
 
+function initializeGame() {
+  initializePerson(dealer);
+  initializePerson(player);
+  dealer.name = 'Dealer';
+  dealer.isDealer = true;
+  player.isDealer = false;
+  player.isStanding = false;
+  player.isAlive = false;
+  player.bet = 0;
+  cardsEl.style.width = dealerEl.style.width = (CARDSLOT * 2) + 'px';
+  deck = initializeDeck();
+  clearCardDisplay(cardsEl);
+  clearCardDisplay(dealerEl);
+  hideCardButtons();
+  hideElement(cardsEl);
+  hideElement(dealerEl);
+  hideElement(dealerText);
+  hideElement(playerText);
+  hideElement(sumEl);
+  messageEl.classList.remove('win','lose','push');
+  sumEl.textContent = `Sum: ${player.total}`;
+  if (document.querySelector('.bet')) document.querySelector('.bet').remove();
+  document.querySelector('.actions').style.justifyContent = 'center';
+  game.isInitialized = true;
+}
+
+function initializePerson(person) {
+  person.hand = [];
+  person.has21 = false;
+  person.hasAce = false;
+  person.hasBlackjack = false;
+  person.isBusted = false;
+  person.total = 0;
+  person.totalWithAce = 0;
+}
+
+function log(msg) {
+  if (LOGGING) console.log(msg);
+}
+
+function renderGame() {
+  displayCards(dealer, dealerEl);
+  if (checkForBlackJack()) return;
+  log('no blackjacks, checking player hand');
+  checkPlayerHand();
+}
+
+function result(result, msg) {
+  messageEl.classList.add(result);
+  messageEl.textContent = msg;
+  player.isAlive = false;
+  hideCardButtons();
+}
+
+function showElement(element) {
+  element.style.visibility = 'visible';
+}
+
+function shuffleDeck() {
+  let newDeck = [];
+  let cardCount = deck.length;
+  for (let i = 0; i < cardCount; i++) {
+    let cardNumber = Math.floor(Math.random() * deck.length);
+    newDeck.push(deck.splice(cardNumber, 1)[0]);
+  }
+  return newDeck;
+}
+
+function stand() {
+  if (player.hasBlackjack || player.has21 || player.isStanding || !player.isAlive) return;
+  player.isStanding = true;
+  if (player.hasAce) {
+    if (player.totalWithAce > 21) player.totalWithAce = player.total;
+    else player.total = player.totalWithAce;
+    log(`player is standing with an ace.  (${player.total}/${player.totalWithAce})`);
+  } else log(`player is standing with ${player.total}/${player.totalWithAce}`);
+
+  checkPlayerHand();
+  checkDealerHand();
+}
+
+function startGame() {
+  if (document.getElementById('limitations')) document.getElementById('limitations').remove();
+  if (!game.isInitialized) {
+    initializeGame();
+  }
+  message = '';
+  game.isInitialized = false;
+  player.isAlive = true;
+  hideElement(startButton);
+  shuffledDeck = shuffleDeck();
+  dealCard(player);
+  dealCard(dealer); //'rigged', 0 is AofS -- 12 is KofS
+  dealCard(player);
+  dealCard(dealer);
+  renderGame();
+}
